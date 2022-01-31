63fc379f48812a7bd8811f3eb4ad4b803247ce0c
Jacek Smolak <jacek.smolak@gmail.com>
2022-01-31 23:07:01 +0100

    Have Jest's global functions (describe, it) visible in tests

--- test/index.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import { foo } from "../index";
-
-describe("foo function", () => {
-  it("should return bar", () => {
-    expect(foo()).to.equal("bar");
-  });
-});
